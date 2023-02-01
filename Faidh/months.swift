b87465872a29df7bdcfd72ee9aca550519a2800f
suimiAA0011 <sm.seemo25@gmail.com>
2023-02-01 19:41:18 +0300

    #9 commit

--- Faidh/months.swift
+++ Faidh/months.swift
@@ -842,3 +842,240 @@ struct inc_Previews: PreviewProvider {
         inc()
     }
 }
+
+
+struct exp: View {
+    @State private var selection = ""
+    @State var toapr:Bool=false
+    @State var forapr:Bool=false
+    @State var backapr:Bool=false
+    let months: [String] = Calendar.current.shortMonthSymbols
+    var body: some View {
+        VStack {
+            ZStack {
+               
+            ZStack {
+                pieChartrender(values: [699,1110], colors: [Color(red: 0.793, green: 0.479, blue: 0.389),Color(red: -0.269, green: 0.619, blue: 0.865)], backgroundColor: Color(red: -0.063, green: 0.184, blue: 0.304), innerRadiusFraction: 0.5).padding(.top, 10.0).offset(x:100,y:70)
+                HStack{
+                    
+                    VStack {
+    //(Color(red: 0.793, green: 0.479, blue: 0.389)
+    //                    Button("Analtyics") {
+    //                        toapr.toggle()
+    //                    }.foregroundColor(Color(red: -0.169, green: 0.372, blue: 0.623))
+    //                        .fontWeight(.semibold)
+    //                        .font(.subheadline)
+    //                        .frame(width: 118, height: 35)
+    //                        .background(Color(.white))
+    //                        .cornerRadius(16)
+    //                        .shadow(radius: 3)
+    //                        .padding(.top, 29.0)
+    //                        .mask(RoundedRectangle(cornerRadius: 10, style: .continuous))
+    //                        .font(Font.custom("Cairo-Bold", size: 24)).padding(.trailing,-5)
+    //                        .offset(x:130,y: 390)
+    //                        .padding()
+    //                        .fullScreenCover(isPresented: $toapr){
+    //                            analysis()}
+                        VStack {
+                            ZStack {
+                                
+                                
+                                
+                
+                              
+                              
+                                HStack {
+                                    RoundedRectangle(cornerRadius: 15).frame(width: 55,height:35).foregroundColor(Color(red: -0.157, green: 0.392, blue: 0.606))
+                                   
+                                        
+                                        
+                                    Text("Housing and bills")
+                                        .foregroundColor(.white)
+                                        .fontWeight(.bold)
+                                        
+                                        
+                                    .padding(.top, 14.0)
+                                }.offset(x: -40, y: -50)
+                                .padding(.trailing, 110.0)
+                                HStack {
+                                    RoundedRectangle(cornerRadius: 15).frame(width: 55,height:35).foregroundColor(Color(red: -0.269, green: 0.619, blue: 0.865)).offset(x:-50)
+                                   
+                                        
+                                        
+                                    Text("Food")
+                                        .foregroundColor(.white)
+                                        .fontWeight(.bold)
+                                        
+                                        
+                                    .padding(.top, 14.0)
+                                }.offset(x: -40, y: -10)
+                                .padding(.trailing, 110.0)
+                                HStack {
+                                    RoundedRectangle(cornerRadius: 15).frame(width: 55,height:35).foregroundColor(Color(red: 0.792, green: 0.479, blue: 0.389))
+                                        .offset(x:-10)
+                                   
+                                        
+                                        
+                                    Text("Transportation")
+                                        .foregroundColor(.white)
+                                        .fontWeight(.bold)
+                                        
+                                        
+                                    .padding(.top, 14.0)
+                                }.offset(x: -40, y: 30)
+                                .padding(.trailing, 110.0)
+                            }
+                            
+                            .padding(.top, 200.0)
+                        }
+                        HStack {
+                            Spacer()
+                            VStack {
+                                ZStack {
+                                    
+                                    
+                                    HStack {
+                                        RoundedRectangle(cornerRadius: 15).frame(width: 55,height:35).foregroundColor(Color(red: 0.802, green: 0.385, blue: 0.397))
+                                        
+                                       
+                                            .offset(x: -90, y: -80)
+                                           
+                                            
+                                        Text("Other")
+                                            .foregroundColor(.white)
+                                            .fontWeight(.bold)
+                                            .offset(x: -40, y: -85)
+                                        .padding(.top, 14.0)
+                                    }.offset(x:-69,y:103)
+                                  
+                                }.padding(.leading, 0.0)
+                            }
+                                
+                                
+                            
+                     
+                            
+                        }.padding(.trailing, 125.049)
+                    }
+                    
+                    
+                    
+                }.padding(.bottom, 16.0)
+               
+            }.padding(.bottom, 76.0)
+                .background(Color(red: 0.0, green: 0.184, blue: 0.308))
+                HStack{
+                    Button {
+                        forapr.toggle()
+                    } label: {
+                        Image(systemName: "chevron.left.circle.fill")
+                            .foregroundColor(.white)
+                            .fontWeight(.bold)
+                            .font(.system(size: 30))
+                            .frame(width: 7,height: 7)
+                            
+                        
+                    
+                    }.fullScreenCover(isPresented: $forapr){
+                        home()}
+                    Text("January")
+                        .foregroundColor(Color(red: -0.169, green: 0.372, blue: 0.623))
+                            .fontWeight(.bold)
+                            .font(.subheadline)
+                            .frame(width: 118, height: 35)
+                            
+                            .cornerRadius(16)
+                            .shadow(radius: 32)
+                    
+                    Button {
+                        forapr.toggle()
+                    } label: {
+                        Image(systemName: "chevron.right.circle.fill")
+                            .foregroundColor(.white)
+                            .fontWeight(.bold)
+                            .font(.system(size: 30))
+                            .frame(width: 7,height: 7)
+                            
+                        
+                    
+                    }.fullScreenCover(isPresented: $forapr){
+                        home()}
+                    
+                }
+                   
+                
+                .offset(x:-10,y:-80)
+                    
+            }.frame(width: 400,height: 5)
+                .background(Color(red: 0.0, green: 0.184, blue: 0.308)).ignoresSafeArea(.all)
+            VStack{
+                HStack{
+                    Text("Income Transactions")
+                        .foregroundColor(.white).fontWeight(.bold)
+                    
+                    Button("SEE ALL"){
+                        
+                    }
+                }.padding(.top, 9.0)
+                
+                    ZStack {
+                        RoundedRectangle(cornerRadius: 17)
+                                 .fill(Color(.white))
+                                 .frame(width: 350, height: 60)
+                             .padding(.trailing, 20.0)
+                        HStack(spacing:20){
+                            Image(systemName: "house.circle")
+                            VStack{
+                                
+                                Text("Side hustle")
+                                Text("Deposite")
+                                    .foregroundColor(.gray)
+                              
+                            }
+                            VStack{
+                              
+                                Text("26 DEC")
+                                    .foregroundColor(.gray)
+                                Text("$+350")
+                            }.padding(.leading, 70.0)
+                        }
+                    }
+                ZStack {
+                    RoundedRectangle(cornerRadius: 17)
+                             .fill(Color(.white))
+                             .frame(width: 350, height: 60)
+                         .padding(.trailing, 20.0)
+                    HStack(spacing:20){
+                       
+                        Image(systemName: "paperplane")
+                        VStack{
+                            Text("Salary")
+                            Text("Deposite")
+                                .foregroundColor(.gray)
+                           
+                        }
+                        VStack{
+                          
+                            Text("25 Jan")
+                                .foregroundColor(.gray)
+                            Text("$+7000")
+                        }.padding(.leading, 70.0)
+                    }
+                }
+               
+                     
+            }.frame(width: 800,height: 100)
+            .background(Color(red: 0.0, green: 0.184, blue: 0.308)).ignoresSafeArea(.all).padding(.top)
+                .offset(x:10,y:200)
+                
+            
+
+        }
+        
+    }
+}
+struct exp_Previews: PreviewProvider {
+    static var previews: some View {
+        exp()
+    }
+}
